e0320c926bd0779845edff2123aca2d1a953f427
Murtaza Kalāgh <94598610+MurtazaKafka@users.noreply.github.com>
2022-08-17 09:25:54 +0430

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'test2';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
